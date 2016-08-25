9d5088c125f987f157ac47057fac6cb44eb02bc5
Gregory Mendes <gremendes@hotmail.com>
2016-08-24 17:05:41 -0700

    Escalonamento de processos

---
+++ Escalonamento.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+    int numProc,tempoExec[20],tempoEspera[20],tempoTurnaround[20],prioridade[20],mediaTempoEspera=0,mediaTempoTurnaround=0,i,j;
+    printf("Quantidade de processos(max 20):");
+    scanf("%d",&numProc);
+
+    printf("\nTempo de execucao e prioridade do processo\n");
+    for(i=0;i<numProc;i++)
+    {
+        printf("P[%d]:",i+1);
+        printf("\nTempo de execucao:");
+        scanf("%d",&tempoExec[i]);
+
+        printf("\nPrioridade:");
+        scanf("%d",&prioridade[i]);
+
+    }
+
+    tempoEspera[0]=0;    //tempo de espera para o primeiro processo eh 0
+
+    //calculando tempo de espera
+    for(i=1;i<numProc;i++)
+    {
+        tempoEspera[i]=0;
+        for(j=0;j<i;j++)
+            tempoEspera[i]+=tempoExec[j];
+    }
+
+    printf("\nProcesso\tTempo Execucao\tTempo Espera\tTurnaround");
+
+    //calculando tempo de turnaround
+    for(i=0;i<numProc;i++)
+    {
+
+        tempoTurnaround[i]=tempoExec[i]+tempoEspera[i];
+        mediaTempoEspera+=tempoEspera[i];
+        mediaTempoTurnaround+=tempoTurnaround[i];
+
+        printf("\nP[%d]\t\t%d\t\t%d\t\t%d",i+1,tempoExec[i],tempoEspera[i],tempoTurnaround[i]);
+        sleep(tempoExec[i]);
+    }
+
+    mediaTempoEspera/=i;
+    mediaTempoTurnaround/=i;
+    printf("\n\nMedia de tempo de espera:%d",mediaTempoEspera);
+    printf("\nMedia turnaround:%d",mediaTempoTurnaround);
+
+    return 0;
+}
